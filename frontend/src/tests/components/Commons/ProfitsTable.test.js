d33d92250234770d41aa0db258f0eccee9f8d2cf
Alec Song <alecsong@Alecs-MacBook-Pro.local>
2024-11-25 18:25:03 -0800

    fixed ProfitsTable.js not having full mutation coverage

--- frontend/src/tests/components/Commons/ProfitsTable.test.js
+++ frontend/src/tests/components/Commons/ProfitsTable.test.js
@@ -3,6 +3,11 @@ import ProfitsTable from "main/components/Commons/ProfitsTable";
 import profitsFixtures from "fixtures/profitsFixtures";
 
 describe("ProfitsTable tests", () => {
+    const testId = "ProfitsTable";
+
+    const expectedHeaders = ["Profit", "Date", "Health", "Cows"];
+    const expectedFields = ["Profit", "date", "Health", "numCows"];
+
     test("renders without crashing for 0 profits", () => {
         render(
             <ProfitsTable profits={[]} />
@@ -16,13 +21,38 @@ describe("ProfitsTable tests", () => {
         await waitFor(()=>{
             expect(screen.getByTestId("ProfitsTable-header-Profit") ).toBeInTheDocument();
         });
-
-        const expectedHeaders = [ "Profit", "Date", "Health", "Cows"];
     
         expectedHeaders.forEach((headerText) => {
           const header = screen.getByText(headerText);
           expect(header).toBeInTheDocument();
         });
 
+    });
+
+    test("renders table correctly with profits data", () => {
+        render(
+            <ProfitsTable profits={profitsFixtures.threeProfits} />
+        );
+        
+        expectedHeaders.forEach((headerText) => {
+            const header = screen.getByText(headerText);
+            expect(header).toBeInTheDocument();
+        });
+
+        expectedFields.forEach((field) => {
+            const fieldElement = screen.getByTestId(`${testId}-cell-row-0-col-${field}`);
+            expect(fieldElement).toBeInTheDocument();
+        });
+
+        profitsFixtures.threeProfits.forEach((profit, rowIndex) => {
+            expect(screen.getByTestId(`${testId}-cell-row-${rowIndex}-col-Profit`))
+                .toHaveTextContent(`$${profit.amount.toFixed(2)}`);
+
+            expect(screen.getByTestId(`${testId}-cell-row-${rowIndex}-col-Health`))
+                .toHaveTextContent(`${profit.avgCowHealth}%`);
+
+            expect(screen.getByTestId(`${testId}-cell-row-${rowIndex}-col-numCows`))
+                .toHaveTextContent(profit.numCows.toString());
+        });
     });
 });
